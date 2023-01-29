ddd55a9561274c8df6fce3496e2fd88266121dae
JuliaMISH <juliamish@mail.ru>
2023-01-29 14:35:58 +0300

    modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-  });
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
+});
+
