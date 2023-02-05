37aa185c2e52c05cc517db9359b4ac0d1bc2475a
JuliaMISH <juliamish@mail.ru>
2023-02-05 12:28:42 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+
     var modal = document.querySelectorAll('.modal');
     var instances_modal = M.Modal.init(modal);
+
+    var gallery = document.querySelectorAll('.materialboxed');
+    var instances_gallery = M.Materialbox.init(gallery);
 });
 
